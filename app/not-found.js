d63affadbefe78206392a212fd5ff18c437d0cc9
Vishal Kumar <vishalkumar@Solvatives-Laptop.local>
2024-07-17 11:43:02 +0530

    fix: cloudnery issue

--- app/not-found.js
+++
@@ -1,6 +0,0 @@
-'use client'
-import TinaPage, {TINA_DEFAULT_PAGES} from '../components/TinaPage'
-
-const Error = () => <TinaPage slug={[TINA_DEFAULT_PAGES[404]]} />
-
-export default Error
--- components/TinaComp.js
+++ components/TinaComp.js
@@ -8,7 +8,11 @@ export const TINA_DEFAULT_PAGES = {
   500: "500.mdx",
 };
 const TinaComp = (props) => {
-  const { data } = useTina(props);
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
   const { page } = data;
   const { section } = page;
   const isFooterVisible = !!page.isFooterVisible;
